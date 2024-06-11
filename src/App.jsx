7648db57b1e3a22555b9105f1fcc9c27f067eb94
MUKKOTI-SHASHANK <mukkoti.shashank@gmail.com>
2024-06-11 18:40:43 +0530

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
     <BrowserRouter>
      <Routes>
       <Route path="/home" element={<Home />} />
-      <Route path="/signup" element={<Signup/>} />
+      <Route path="/" element={<Signup/>} />
       <Route path="/login" element={<Login/>} />
      </Routes>
     </BrowserRouter>
